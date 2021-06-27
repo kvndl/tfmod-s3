94276cd5d60bfcbd9948250519161e8e79d8088d
Noah <admin@noahschepers.com>
2021-06-27 19:33:02 -0400

    Remove useless outputs

--- Module/output.tf
+++ Module/output.tf
@@ -1,11 +1,3 @@
 output "bucket_name" {
   value = aws_s3_bucket.service_bucket.id
 }
-
-output "bucket_access_key" {
-  value = aws_iam_access_key.service_bucket_user_access_key.id
-}
-
-output "bucket_secret_key" {
-  value = aws_iam_access_key.service_bucket_user_access_key.secret
-}
